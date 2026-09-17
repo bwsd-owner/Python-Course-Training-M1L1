64afa71ce870667db8786e54ceda7820669afac8
setl-intercube <avisheak.cse@std.cu.ac.bd>
2026-09-17 10:16:47 +0600

    Ac1 commit

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-print("Trying something again!!")
-print("How many continent are there in the world??")
+print("Hello Coders!")
 
-print("1. 6")
-print("2. 7")
-print("3. 8")
+print("My name is Avisheak.")
 
-choice = input("Enter option 1, 2 or 3: ")
+print("I love playing Chess.")
 
-if choice == "2":
-    print("Congrats!!")
-else: 
-    print("Try Again mate!")
+print("The result of 3 * 5 is:", 3 * 5)
