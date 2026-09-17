cb751f1c2ef7175bcef8ef786bb28c5ca1949942
setl-intercube <avisheak.cse@std.cu.ac.bd>
2026-09-17 10:10:39 +0600

    New Commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Trying something again!!")
+print("How many continent are there in the world??")
+
+print("1. 6")
+print("2. 7")
+print("3. 8")
+
+choice = input("Enter option 1, 2 or 3: ")
+
+if choice == "2":
+    print("Congrats!!")
+else: 
+    print("Try Again mate!")
